49665ae4e80d2877db9a58c2534cd34d9fb4960f
4Freak <pasha.bobrik@bk.ru>
2022-11-22 01:33:12 +0300

    Add validate finite automaton

--- String Formatter/Tests/StringFormatterTests.cs
+++ String Formatter/Tests/StringFormatterTests.cs
@@ -1,3 +1,7 @@
+using String_Formatter;
+using String_Formatter.Interfaces;
+using System.ComponentModel.DataAnnotations;
+
 namespace Tests
 {
 	public class StringFormatterTests
@@ -8,8 +12,34 @@ namespace Tests
 		}
 
 		[Test]
-		public void Test1 ()
+		public void StringValidatorTest ()
 		{
+			var stringValidator = new StringValidator();
+			var ids = stringValidator.IsValid("string");
+			ids = stringValidator.IsValid("{id}");
+			ids = stringValidator.IsValid("SomeText {id} some text");
+			ids = stringValidator.IsValid("SomeText {id} some text {_id2}");
+			ids = stringValidator.IsValid("{{CorrectId}}");
+			try
+			{
+				ids = stringValidator.IsValid("Some text {{0InCorrectId}}");
+			}
+			catch{ };
+			try
+			{
+				ids = stringValidator.IsValid("}{");
+			}
+			catch{ };
+			try
+			{
+				ids = stringValidator.IsValid("{{");
+			}
+			catch{ }
+			try
+			{ 
+				ids = stringValidator.IsValid("}");
+			}
+			catch{ } 
 			Assert.Pass();
 		}
 	}
